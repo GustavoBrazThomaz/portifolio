8b584dbc1c6480cc5ba76794e8c4d70998a87cc7
GustavoBrazThomaz <gustavo.brazthomaz@gmail.com>
2022-08-31 15:43:58 -0300

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -48,7 +48,7 @@ function Projects() {
   },{
     name: 'PetShop',
     description: 'PetShop é um app desenvolvido em React com Typescript utilizando a Rest API em Node Js e Mongo db ',
-    link: 'https://vercel.com/gustavobrazthomaz/petshop-react',
+    link: 'https://petshop-react-blue.vercel.app/',
     image: 'https://cdn.discordapp.com/attachments/366606804018397194/1014597713670713364/unknown.png',
     working: false,
     template: true
